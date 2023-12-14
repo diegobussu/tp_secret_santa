d5962cedcee0f2d0e5dd5371fb99c2c64ca71c0c
DontitoSama <jokonito06@gmail.com>
2023-12-14 16:23:38 +0100

    add swagger config

---
+++ swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0', 
+    info: {
+      title: 'API N°1',
+      version: '1.0.0',
+      description: 'API USING EXPRESS LIBRARY AND MONGODB DATABASE WITH A MIDDLEWARE',
+    },
+  },
+
+  apis: ['./routes/*.js'],
+};
+
+// Initialize Swagger
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = {
+  serveSwaggerUI: swaggerUi.serve,
+  setupSwaggerUI: swaggerUi.setup(swaggerSpec),
+};
